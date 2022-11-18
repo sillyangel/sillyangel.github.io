4ecb21348187f5575c70622096e4ebacc476ff0e
sillyangel <sillyangel3@gmail.com>
2022-11-18 15:15:37 +0000

    a

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+
+const toast = document.getElementById('toast');
+
+function a() {
+  toast.querySelector('.toast-body').innerHTML = "Toast notification is working ...";
+  toast.classList.add('visible');
+}
+function b() {
+  toast.classList
+}
+document.getElementById('hideBtn').addEventListener('click', function() {
+    toast.classList.remove('visible');
+});
