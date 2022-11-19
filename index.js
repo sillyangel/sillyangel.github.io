cc8b5d247ecc852759e3ef130ee34da0da26ee1f
sillyangel <sillyangel3@gmail.com>
2022-11-19 21:40:55 +0000

    added cookies

--- index.js
+++ index.js
@@ -16,3 +16,36 @@ function launch_toast() {
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
 }
 
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("offlinenof");
+  if (user != "") {
+    console.log("user saw the thing");
+  } else {
+    user = "seen";
+    setCookie("offlinenof", user, 30);
+    launch_toast();
+  }
+}
